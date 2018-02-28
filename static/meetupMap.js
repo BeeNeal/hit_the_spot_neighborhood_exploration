cc98da77f63ec2ec386c0c765b63bf3b50a824cb
Brittany <brittanyneal22@gmail.com>
2018-02-27 22:37:21 -0800

    working makers on meetup page

--- static/meetupMap.js
+++ static/meetupMap.js
@@ -12,10 +12,6 @@ var meetupMap = new mapboxgl.Map({
 
 console.log($("#LonLat").data('center'));
 
-// what I need to do here - 
-// 2) make a 'Feature' object that duplicates below feature
-
-// 
 // marker mill - getting coordinates from coded hidden divs 
 let markerList = [];
 
@@ -26,8 +22,8 @@ $('.location').each(function(i, obj) {
             geometry: {
                 type: 'Point',
                 coordinates: [
-                  obj.dataset.lat,
                   obj.dataset.lon,
+                  obj.dataset.lat,
                 ]
             },
             properties: {
@@ -38,9 +34,7 @@ $('.location').each(function(i, obj) {
             }
         };
 
-    // x.push(obj);
     markerList.push(uniqueMarker);
-    // console.log(obj['dataset']['lat'])
 });
 
 var geojson = {
@@ -49,84 +43,6 @@ var geojson = {
 };
 geojson.features = markerList;
 console.log(geojson);
-
-
-// $(".location").each(function(i, obj) {
-//   x.push(mObj)
-// });
-
-//   var mObj = {'lon': $(this).data('lon'), 'lat': $(this).data('lat')}
-// console.log(mObj)
-
-  // var coords = ['$(this).data('lon')', '$(this).data('lat')'];
-  //   var m1 = {
-  //       coordinates: coords,
-  //       name: $(this).data('name');
-  //   };
-  //   markerList.push(m1)
-
-
- // let x = [$(".location").each]
-// var features = [];
-
-//     for (var i=0; i< x.length; i++){
-//         var uniqueLocation = x[i];
-//         uniqueMarker = {
-//             type: 'Feature',
-//             geometry: {
-//                 type: 'Point',
-//                 coordinates: [
-//                   uniqueLocation.lat,
-//                   uniqueLocation.lon
-//                 ]
-//             },
-//             properties: {
-//                 title: uniqueLocation.address,
-//                 'marker-symbol': 'pitch',
-//                 'marker-size': 'large',
-//                 'marker-color': '#FF0066',
-//             }
-        // };
-    //     features.push(uniqueMarker);
-    // }
-
-
-
-
-
-    // ['lon': '$(this).data('lon')', 'lat': '$(this).data('lat')']
-console.log(markerList)
-
-    // console.log($(this).data('lon'));
-// need to loop through the above list of lat lons and make new features with these
-
-// geojson = []
-
-var geojson = {
-  type: 'FeatureCollection',
-  features: [{
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [-122.411981039, 37.8013699309]
-    },
-    properties: {
-      title: 'Test',
-      description: 'Test'
-    }
-  },
-  {
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [-122.414, 37.776]
-    },
-    properties: {
-      title: 'Mapbox',
-      description: 'San Francisco, California'
-    }
-  }]
-};
 
 // add markers to map
 geojson.features.forEach(function(marker) {
